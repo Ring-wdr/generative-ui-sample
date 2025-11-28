d85f153a07e0525a372cb97a90270ff5e7ba417c
ring-wdr <akswnd55@gmail.com>
2025-11-28 23:11:25 +0900

    chore: stylex installed

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import stylexPlugin from "unplugin-stylex/rspack";
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()],
+	plugins: [pluginReact()],
+	tools: {
+		rspack: {
+			plugins: [stylexPlugin()],
+		},
+	},
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
 
+import * as stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  button: {
+    color: 'red',
+    backgroundColor: 'blue',
+  }
+})
+
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <button {...stylex.props(styles.button)}>Click me</button>
     </div>
   );
 };
